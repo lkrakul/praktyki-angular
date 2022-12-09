30aa62edd23cb05dfaa7c6860974f6b1409ddb7a
Łukasz Kuś <lukasz23kus@gmail.com>
2022-12-09 10:58:43 +0100

    add currency service

--- src/app/components/kalkulator-walut/kalkulator-walut.component.ts
+++ src/app/components/kalkulator-walut/kalkulator-walut.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CurrencyService } from '../../services/currency.service';
 
 @Component({
   selector: 'app-kalkulator-walut',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./kalkulator-walut.component.scss']
 })
 export class KalkulatorWalutComponent {
-  
+  constructor(private currencyService: CurrencyService) {
+    this.currencyService.convertCurrency("GBP", "PLN").subscribe(response => {
+      console.log(response.result);
+    });
+  }
 }
