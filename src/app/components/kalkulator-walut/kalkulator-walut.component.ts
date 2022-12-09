9e80ecf0e2eb473bf8a719c4def38ec1e2406ba6
Michał <michal.kus.2004@gmail.com>
2022-12-09 14:53:39 +0100

    kalkulator walut

--- src/app/components/kalkulator-walut/kalkulator-walut.component.ts
+++ src/app/components/kalkulator-walut/kalkulator-walut.component.ts
@@ -7,9 +7,29 @@ import { CurrencyService } from '../../services/currency.service';
   styleUrls: ['./kalkulator-walut.component.scss']
 })
 export class KalkulatorWalutComponent {
+  public amountMoney: number = 0;
+  public fromCurrency: string = "PLN";
+  public toCurrency: string = "USD";
+  public exchangeRate: string = "";
+  public result: string = "";
+
   constructor(private currencyService: CurrencyService) {
-    this.currencyService.convertCurrency("GBP", "PLN").subscribe(response => {
-      console.log(response.result);
+
+  }
+
+  public calculate() {
+    console.log(this.fromCurrency)
+  }
+
+  public convertCurrency() {
+    this.currencyService.convertCurrency(this.fromCurrency, this.toCurrency).subscribe(response => {
+      let total = this.amountMoney * response.result
+      this.exchangeRate = `1 ${this.fromCurrency} = ${response.result} ${this.toCurrency}`
+      if (this.toCurrency == "BTC") {
+        this.result = `${this.amountMoney} ${this.fromCurrency} = ${total} ${this.toCurrency}`
+      } else {
+        this.result = `${this.amountMoney} ${this.fromCurrency} = ${total.toFixed(2)} ${this.toCurrency}`
+      }
     });
   }
 }
